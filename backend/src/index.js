f420c03e95802033c4e23d390d6c7f1355460a5d
aditya8957 <alphaaditya123@gmail.com>
2026-02-14 22:29:45 +0530

    Cors update

--- backend/src/index.js
+++ backend/src/index.js
@@ -16,7 +16,7 @@ const cors = require('cors')
 app.use(cors({
     origin: [
         "http://localhost:5173",
-        "https://codearena.vercel.app"
+        "hhttps://codearena-two.vercel.app/"
     ],
     credentials: true
 }));
