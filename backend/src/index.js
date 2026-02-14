b9b2e795f42dc4333b5ee97fd0ac0726b0c056d7
aditya8957 <alphaaditya123@gmail.com>
2026-02-14 20:56:34 +0530

    Fixed dynamic port for Render

--- backend/src/index.js
+++ backend/src/index.js
@@ -28,7 +28,7 @@ app.use('/ai',aiRouter);
 app.use("/video",videoRouter);
 
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 5000;
 
 const InitalizeConnection = async ()=>{
     try{
